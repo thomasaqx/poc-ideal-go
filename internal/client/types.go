ad49b30bce497b92226e5776e6c7042850324e51
Thomas <thomas.queiroz@idealctvm.com.br>
2025-11-24 11:56:25 -0300

    feat: simplify Quote struct by removing unused fields

--- internal/client/types.go
+++ internal/client/types.go
@@ -8,84 +8,13 @@ type QuoteResponse struct {
 	} `json:"quoteResponse"`
 }
 
+// Quote contains only the fields we expose to the API.
 type Quote struct {
-	Language                          string  `json:"language"`
-	Region                            string  `json:"region"`
-	QuoteType                         string  `json:"quoteType"`
-	TypeDisp                          string  `json:"typeDisp"`
-	QuoteSourceName                   string  `json:"quoteSourceName"`
-	Triggerable                       bool    `json:"triggerable"`
-	CustomPriceAlertConfidence        string  `json:"customPriceAlertConfidence"`
-	Currency                          string  `json:"currency"`
-	MarketState                       string  `json:"marketState"`
-	RegularMarketChangePercent        float64 `json:"regularMarketChangePercent"`
-	RegularMarketPrice                float64 `json:"regularMarketPrice"`
-	Exchange                          string  `json:"exchange"`
-	ShortName                         string  `json:"shortName"`
-	LongName                          string  `json:"longName"`
-	MessageBoardId                    string  `json:"messageBoardId"`
-	ExchangeTimezoneName              string  `json:"exchangeTimezoneName"`
-	ExchangeTimezoneShortName         string  `json:"exchangeTimezoneShortName"`
-	GmtOffSetMilliseconds             int     `json:"gmtOffSetMilliseconds"`
-	Market                            string  `json:"market"`
-	EsgPopulated                      bool    `json:"esgPopulated"`
-	RegularMarketChange               float64 `json:"regularMarketChange"`
-	RegularMarketTime                 int64   `json:"regularMarketTime"`
-	RegularMarketDayHigh              float64 `json:"regularMarketDayHigh"`
-	RegularMarketDayRange             string  `json:"regularMarketDayRange"`
-	RegularMarketDayLow               float64 `json:"regularMarketDayLow"`
-	RegularMarketVolume               int64   `json:"regularMarketVolume"`
-	RegularMarketPreviousClose        float64 `json:"regularMarketPreviousClose"`
-	Bid                               float64 `json:"bid"`
-	Ask                               float64 `json:"ask"`
-	BidSize                           int     `json:"bidSize"`
-	AskSize                           int     `json:"askSize"`
-	FullExchangeName                  string  `json:"fullExchangeName"`
-	FinancialCurrency                 string  `json:"financialCurrency"`
-	RegularMarketOpen                 float64 `json:"regularMarketOpen"`
-	AverageDailyVolume3Month          int64   `json:"averageDailyVolume3Month"`
-	AverageDailyVolume10Day           int64   `json:"averageDailyVolume10Day"`
-	FiftyTwoWeekLowChange             float64 `json:"fiftyTwoWeekLowChange"`
-	FiftyTwoWeekLowChangePercent      float64 `json:"fiftyTwoWeekLowChangePercent"`
-	FiftyTwoWeekRange                 string  `json:"fiftyTwoWeekRange"`
-	FiftyTwoWeekHighChange            float64 `json:"fiftyTwoWeekHighChange"`
-	FiftyTwoWeekHighChangePercent     float64 `json:"fiftyTwoWeekHighChangePercent"`
-	FiftyTwoWeekLow                   float64 `json:"fiftyTwoWeekLow"`
-	FiftyTwoWeekHigh                  float64 `json:"fiftyTwoWeekHigh"`
-	FiftyTwoWeekChangePercent         float64 `json:"fiftyTwoWeekChangePercent"`
-	DividendDate                      int64   `json:"dividendDate"`
-	EarningsTimestamp                 int64   `json:"earningsTimestamp"`
-	EarningsTimestampStart            int64   `json:"earningsTimestampStart"`
-	EarningsTimestampEnd              int64   `json:"earningsTimestampEnd"`
-	EarningsCallTimestampStart        int64   `json:"earningsCallTimestampStart"`
-	EarningsCallTimestampEnd          int64   `json:"earningsCallTimestampEnd"`
-	IsEarningsDateEstimate            bool    `json:"isEarningsDateEstimate"`
-	TrailingAnnualDividendRate        float64 `json:"trailingAnnualDividendRate"`
-	TrailingPE                        float64 `json:"trailingPE"`
-	TrailingAnnualDividendYield       float64 `json:"trailingAnnualDividendYield"`
-	EpsTrailingTwelveMonths           float64 `json:"epsTrailingTwelveMonths"`
-	EpsForward                        float64 `json:"epsForward"`
-	EpsCurrentYear                    float64 `json:"epsCurrentYear"`
-	PriceEpsCurrentYear               float64 `json:"priceEpsCurrentYear"`
-	SharesOutstanding                 int64   `json:"sharesOutstanding"`
-	BookValue                         float64 `json:"bookValue"`
-	FiftyDayAverage                   float64 `json:"fiftyDayAverage"`
-	FiftyDayAverageChange             float64 `json:"fiftyDayAverageChange"`
-	FiftyDayAverageChangePercent      float64 `json:"fiftyDayAverageChangePercent"`
-	TwoHundredDayAverage              float64 `json:"twoHundredDayAverage"`
-	TwoHundredDayAverageChange        float64 `json:"twoHundredDayAverageChange"`
-	TwoHundredDayAverageChangePercent float64 `json:"twoHundredDayAverageChangePercent"`
-	MarketCap                         int64   `json:"marketCap"`
-	ForwardPE                         float64 `json:"forwardPE"`
-	PriceToBook                       float64 `json:"priceToBook"`
-	SourceInterval                    int     `json:"sourceInterval"`
-	ExchangeDataDelayedBy             int     `json:"exchangeDataDelayedBy"`
-	AverageAnalystRating              string  `json:"averageAnalystRating"`
-	Tradeable                         bool    `json:"tradeable"`
-	CryptoTradeable                   bool    `json:"cryptoTradeable"`
-	Symbol                            string  `json:"symbol"`
+	Symbol               string  `json:"symbol"`
+	RegularMarketPrice   float64 `json:"regularMarketPrice"`
+	RegularMarketDayHigh float64 `json:"regularMarketDayHigh"`
+	RegularMarketDayLow  float64 `json:"regularMarketDayLow"`
 }
-
 
 type HistoricalDataResponse struct {
 	Chart struct {
